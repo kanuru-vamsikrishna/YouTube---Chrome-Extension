24cf2002c6c729755f60728bca8b498c41dbbd0f
kanuru-vamsikrishna <kanuruvamsikrishna9999@gmail.com>
2025-01-13 12:39:50 +0530

    Adding bookmark button for a new video and ability to get current time stamp

--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,57 @@
 (() => {
   let youtubeLeftControls, youtubePlayer;
   let currentVideo = "";
+  let currentVideoBookmarks = []
 
-  chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    const { type, value, videoId } = obj;
+  if (window.hasRun) {
+    return;
+  }
+  window.hasRun = true;
+  console.log(window, "window");
 
-    if (type === "NEW") {
-      currentVideo = videoId
-      
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+      const { type, value, videoId } = obj;
+
+      if (type === "NEW") {
+        currentVideo = videoId
+        console.log('123');
+        newVideoLoaded()
+      }
+    })
+
+  const newVideoLoaded = () => {
+    const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0]
+    console.log(bookmarkBtnExists, "bookmarkBtnExists")
+    if (!bookmarkBtnExists) {
+      const bookmarkBtn = document.createElement("img")
+      bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+      bookmarkBtn.className = "ytp-button" + "bookmark-btn";
+      bookmarkBtn.title = "Click to bookmark current timestamp";
+
+      youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0]
+      youtubePlayer = document.getElementsByClassName("video-stream")[0]
+
+      youtubeLeftControls.appendChild(bookmarkBtn);
+      bookmarkBtn.addEventListener("click", addNewBookmark)
     }
+  }
 
-  })
+  const addNewBookmark = () => {
+    const currentTime = youtubePlayer.currentTime;
+    const newBookmark = {
+      time: currentTime,
+      desc: "Bookmark at " + getTime(currentTime)
+    }
+    console.log(newBookmark, "newBookmark")
+    chrome.storage.sync.set({
+      [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a - b))
+    })
+  }
 
 })()
+
+const getTime = (time) => {
+  let date = new Date(0);
+  date.setSeconds(time)
+  return date.toISOString().substring(11, 8)
+}
